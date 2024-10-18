f16560bc3ca3763bf51fea5fc7e0d4f1d9c9b5ae
jack456740 <alvin.rogers@bison.howard.edu>
2024-10-17 17:43:42 -0700

    https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html
    https://docs.oracle.com/javase/tutorial/collections/

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question3/VotingMachine.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question3/VotingMachine.java
@@ -1,10 +1,36 @@
 package org.howard.edu.lsp.midterm.question3;
 
-public class HelloWorld {
+import java.util.HashMap;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class VotingMachine {
+ private HashMap<String, Integer> candidates;
 
-	}
+ public VotingMachine() {
+     candidates = new HashMap<>();
+ }
 
+ // Add a new candidate with 0 votes
+ public void addCandidate(String name) {
+     candidates.put(name, 0);
+ }
+
+ // Increment vote count for the candidate
+ public void castVote(String name) {
+     if (candidates.containsKey(name)) {
+         candidates.put(name, candidates.get(name) + 1);
+     } else {
+         System.out.println("Candidate not found.");
+     }
+ }
+
+ // Return the number of votes each candidate has received
+ @Override
+ public String toString() {
+     StringBuilder result = new StringBuilder();
+     for (String name : candidates.keySet()) {
+         result.append("Candidate: ").append(name)
+               .append(", Votes: ").append(candidates.get(name)).append("\n");
+     }
+     return result.toString();
+ }
 }
