b80fe2ce91a89dd1082ea71b5d3bb9a3e4a69178
jack456740 <alvin.rogers@bison.howard.edu>
2024-10-17 17:07:57 -0700

    Book change

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/Main.java
@@ -0,0 +1,9 @@
+package org.howard.edu.lsp.midterm.question1;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book("1984", "George Orwell", "123456789", 1949);
+        System.out.println(book.toString());
+    }
+}
