e75aaa241ddec0cd8e00076cd8d7d43379722d66
jack456740 <alvin.rogers@bison.howard.edu>
2024-10-17 17:27:26 -0700

    Book changes

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/Book.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question1/Book.java
@@ -1,9 +1,36 @@
 package org.howard.edu.lsp.midterm.question1;
 
+public class Book {
+    private String title, author, ISBN;
+    private int yearPublished;
 
-public class Main {
-    public static void main(String[] args) {
-        Book book = new Book("1984", "George Orwell", "123456789", 1949);
-        System.out.println(book.toString());
+    public Book(String title, String author, String ISBN, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getISBN() { return ISBN; }
+    public int getYearPublished() { return yearPublished; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+    public void setISBN(String ISBN) { this.ISBN = ISBN; }
+    public void setYearPublished(int yearPublished) { this.yearPublished = yearPublished; }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book other = (Book) obj;
+        return ISBN.equals(other.ISBN) && author.equals(other.author);
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
     }
 }
